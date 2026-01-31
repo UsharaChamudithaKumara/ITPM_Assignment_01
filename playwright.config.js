f4011a74775a374b338bc8b5db8a9108e5c9f9c5
Ushara Chamuditha Kumara <usharakumara20030820@gmail.com>
2026-01-31 23:42:02 +0530

    Add files via upload

---
+++ playwright.config.js
@@ -0,0 +1,30 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+
+  reporter: [
+    ['html', { open: 'never' }]
+  ],
+
+  use: {
+    headless: false,
+    baseURL: 'https://www.swifttranslator.com/',
+    viewport: { width: 1280, height: 720 }
+  },
+
+  projects: [
+    {
+      name: 'Chromium',
+      use: { ...devices['Desktop Chrome'] }
+    },
+    {
+      name: 'Firefox',
+      use: { ...devices['Desktop Firefox'] }
+    },
+    {
+      name: 'WebKit',
+      use: { ...devices['Desktop Safari'] }
+    }
+  ]
+});
